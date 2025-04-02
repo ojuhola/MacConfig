f9aea23736f13f6379eb8ff96bc2d0f3a806fa0d
Olli-Pekka <ojuhola@hotmail.com>
2025-04-02 21:44:31 +0300

    Fish shell customizations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,10 +2,11 @@ if not status --is-interactive
     exit
 end
 
-##########################
-# My custom commands
+####################################################################################
+# My custom fish shell configurations
 #
 # 2.04.2025 OP
+####################################################################################
 
 # Suppresses the default fish prompt
 set fish_greeting
@@ -17,14 +18,16 @@ function reload
     echo "reloading: $config"
 end
 
+# Prompt for verification before executing commands
 alias rm="rm -i"
 alias mv="mv -i"
 
-# Starship prompt
+# Starship prompt initialization
 if command -sq starship
     starship init fish | source
 end
 
+# eza for directory listings
 if type -q eza
     alias l="eza -1 --icons --no-user --color=always --group-directories-first --git-ignore"
     alias ls="eza -l --icons --no-user --color=always --group-directories-first --git-ignore"
@@ -32,8 +35,10 @@ if type -q eza
     alias lt="eza -la --icons --no-user  --color=always --group-directories-first -T -D --git-ignoreD "
 end
 
+# Brew update and upgrade command
 alias update="brew update && brew upgrade"
 
+# Decorations at the beginning of the terminal
 fastfetch
 
 
