d9bd608f0956e9e71cd0e78e2c9bd58a4cf23b13
Olli-Pekka <ojuhola@hotmail.com>
2025-06-02 18:21:50 +0300

    Fish shell improments

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,9 @@
-####################################################################################
-# config.fish
+################################################################################
 # My custom fish shell configurations
+# 
 #
 # 02.04.2025 OP
-####################################################################################
+################################################################################
 
 # Check if fish is running interactively
 if not status --is-interactive
@@ -17,6 +17,18 @@ function reload
     echo "reloading: $config"
 end
 
+# Function to add and commit changes with a message
+function g.all
+    set -l message (string join " " $argv)
+    if test -z "$message"
+        echo "Error: Please provide a commit message."
+        return 1
+    end
+    git add --all
+    git commit -m "$message"
+    git push
+end
+
 # Replace ls-command with eza for directory listings
 if type -q eza
     alias l="eza -1 --icons --no-user --color=always --group-directories-first --git-ignore"
@@ -24,8 +36,6 @@ if type -q eza
     alias ll="eza -la --icons --no-user --color=always --group-directories-first --git-ignore --time-style='+%d.%m.%y %H:%M'"
     alias lt="eza -la --icons --no-user  --color=always --group-directories-first -T -D --git-ignore"
 end
-
-
 
 # Aliases for common commands
 alias update="brew update --quiet && brew upgrade --greedy --quiet && brew cleanup --quiet && echo -e '==> Done'"
@@ -50,18 +60,6 @@ alias g.lastcommit="git for-each-ref --sort=-committerdate refs/heads/ --format=
      - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
 alias g.discard="git reset --hard; git clean -df"
 
-# Function to add and commit changes with a message
-# Usage: gacp "Your commit message"
-function g.acp
-    set -l message (string join " " $argv)
-    if test -z "$message"
-        echo "Error: Please provide a commit message."
-        return 1
-    end
-    git add --all
-    git commit -m "$message"
-    git push
-end
 # Suppress the default fish greeting
 set -g fish_greeting
 
